e588069855478b2525b7a51cdcca79d5bab182c1
danielruss <druss@mail.nih.gov>
2022-08-29 16:24:21 -0400

    added makeDetailedBarChart

--- R/connectBQ.R
+++ R/connectBQ.R
@@ -17,7 +17,7 @@ connect_bigquery_info <- list(
 #' @param project - The project which is billed for the BQ processing.
 #' @param connectEnv  - Either  "dev", "stage", or "prod"
 #'
-#' @return
+#' @return tibble with data.
 #' @export
 #'
 getOccupationData <- function(project,connectEnv=c("dev","stage","prod")){
@@ -37,11 +37,69 @@ or d_118061122 is not null or d_279637054 is not null) " )
 }
 
 
+#' Pivot the Occupational data
+#'
+#' @param data Occupational data from getOccupationData
+#' @param ...  alternatively, pass in the variables for getOccupationalData and this function
+#'  will call getOccupationalData and pivot in one step
+#'
+#' @return a tibble with Connect Id, longest/Current job, JobTitle, Selection, and rank (1-4,5+)
+#' @export
+#'
+pivotOccupationData<-function(data,...){
+  if (missing("data")){
+    data <- getOccupationData(...)
+  }
+
+  levels =c("1","2","3","4","5+")
+  data <- data %>% tidyr::pivot_longer(2:5, names_to = c("set",".value"), names_sep = 7) %>%
+    dplyr::filter(!is.na(JobTitle)&!is.na(Selection)) %>%
+    dplyr::filter(grepl("NONE_OF_THE_ABOVE|\\d{2}-\\d{4}-\\d",Selection))  %>%
+    dplyr::mutate(rank = factor(dplyr::case_when(
+      Selection == "NONE_OF_THE_ABOVE" ~ "5+",
+      grepl("-0$",Selection) ~ "1",
+      grepl("-1$",Selection) ~ "2",
+      grepl("-2$",Selection) ~ "3",
+      grepl("-3$",Selection) ~ "4",
+    ),levels=levels,ordered = TRUE))
+  attr(data,"date") <- format(Sys.time(),"%a %b %d %Y %I:%M %p")
+  return(data)
+}
+
+
+#' @rdname  makeOccupationBarChart
+makeDetailedOccupationBarChart<-function(pivottedData,...){
+  if (missing(pivottedData)){
+    pivottedData <- pivotOccupationData(...)
+  }
+
+  pivottedData %>% ggplot2::ggplot(ggplot2::aes(x = rank)) + ggplot2::geom_bar() +
+    ggplot2::geom_text(stat = "count",
+                       ggplot2::aes(label = paste0(..count..," (", round(..count.. /sum(..count..) * 100, 1), "%)")),
+                       vjust = -1) +
+    ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0,0.1))) +
+    ggplot2::labs(title = "Connect Participants Selecting from the Top 4\nSOCcer choices",
+                  subtitle = "Current and Longest Job",
+                  caption = paste0("Analysis run on data available at ",attr(pivottedData, "date")), x = "Participant Selected One of the Top 4 SOCcer choices")
+}
+
+
 #' Makes a bar chart for the Occupational Data Downloaded from the Connect Data
 #'
+#' @description
 #' Makes a bar chart that from the Occupational Data in Connect.
 #'
+#' makeOccupationBarChart() returns a bar chart displaying whether or not a job was
+#' coded to the top 4.
+#'
+#' makeDetailedOccupationBarChart() returns a bar chart showing the count of the rank for
+#' all the Connect data.  The count is slight lower in this plot because the first 324
+#' jobs did not have the rank.
+#'
 #' @param occData The occupational data returned from  \code{\link{getOccupationData}}.
+#' @param pivottedData Occupational data returned from \code{\link{pivotOccupationData}}.
+#' @param ... Parameters passed to getOccupationData(), must include both and project,connectEnv
+#'
 #'
 #' @return
 #' @export
@@ -62,3 +120,6 @@ makeOccupationBarChart <- function(occData){
 
 prod_env <- new.env(parent = emptyenv())
 prod_env$module1 <- "`nih-nci-dceg-connect-prod-6d04.flat.module1_scheduledqueries`"
+
+
+

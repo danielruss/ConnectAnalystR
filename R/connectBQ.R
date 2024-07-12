c7198db6dae2c723d78d5007168221caa4be4634
Daniel Russ <druss@mail.nih.gov>
2024-07-12 17:32:26 -0400

    fixed V1/V2 on stage/dev added industry

--- R/connectBQ.R
+++ R/connectBQ.R
@@ -1,15 +1,52 @@
 
 connect_bigquery_info <- list(
   dev = list(
-    module1 = "`nih-nci-dceg-connect-dev.Connect.module1`"
+    module1_v2 = "`nih-nci-dceg-connect-dev.Connect.module1_v2`"
       ),
   stage = list(
-    module1 = "`nih-nci-dceg-connect-stg-5519.flat.flatmodule1_scheduledqueries`"
+    module1_v1 = "`nih-nci-dceg-connect-stg-5519.Connect.module1_v1`",
+    module1_v2 = "`nih-nci-dceg-connect-stg-5519.Connect.module1_v2`"
     ),
   prod =list(
     module1_v1 = "`nih-nci-dceg-connect-prod-6d04.Connect.module1_v1`",
     module1_v2 = "`nih-nci-dceg-connect-prod-6d04.Connect.module1_v2`")
 )
+
+# make a small data dictionary that maps to concept ids needed by this function
+cid <- list(
+  "Connect_ID"="Connect_ID",
+  "CurrentJobTitle"="d_627122657",
+  "CurrentSelection"="d_761310265",
+  "CurrentJobTask"="d_796828094",
+  "LongestJobTitle"="d_118061122",
+  "LongestSelection"="d_279637054",
+  "LongestJobTask"="d_518387017",
+  "CurrentIndustry"="d_700374192.d_700374192",
+  "CurrentIndustryOther"="d_700374192.d_923333992",
+  "LongestIndustry"="d_530742915.d_530742915",
+  "LongestIndustryOther"="d_530742915.D_739237614",
+  "641572847"="Manufacturing",
+  "592592455"="Retail or wholesale sales",
+  "149230791"="Transportation, warehousing, and utilities (e.g., water, sanitation, electric)",
+  "439857718"="Professional and business services (e.g., real estate, technical and scientific services, finance, insurance)",
+  "133362151"="Construction or equipment repair",
+  "372993567"="Mining, quarrying, and oil and gas extraction",
+  "927863729"="Farming, fishing, or forestry",
+  "709307391"="Accommodation and food services (e.g., hotels and restaurants)",
+  "766533183"="Healthcare or social assistance",
+  "698814077"="Government",
+  "942545134"="Military, police, firefighting, security services",
+  "924908599"="Education",
+  "868510850"="Arts, entertainment, and recreation",
+  "807835037"="Some other type of business",
+  "711138281"="Wholesale or distributor",
+  "596792238"="Service Provider",
+  "620577362"="Farming",
+  "751475124"="Military",
+  "178420302"="Don't Know"
+)
+
+
 
 #' Get Occupation data from the Connect Data.
 #'
@@ -26,22 +63,59 @@ getOccupationData <- function(project=preferences$project,env=preferences$env){
   if (is.null(project)) stop('project must be defined')
   if (is.null(env) || !env %in% c("dev","stage","prod")) stop('env must one of "dev", "stage", or "prod"')
 
-  ### need to convert this to a list of queries...
-  v1_query = paste0("SELECT DISTINCT Connect_ID,d_627122657,d_761310265,d_118061122,d_279637054 FROM ",
-                 connect_bigquery_info[[env]]$module1_v1," where Connect_ID is not null AND (d_627122657 is Not null or d_761310265 is not null or d_118061122 is not null or d_279637054 is not null) " )
-  v2_query = paste0("SELECT DISTINCT Connect_ID,d_627122657,d_761310265,d_118061122,d_279637054 FROM ",
-                    connect_bigquery_info[[env]]$module1_v2," where Connect_ID is not null AND (d_627122657 is Not null or d_761310265 is not null or d_118061122 is not null or d_279637054 is not null) " )
 
-  tb1 <- bigrquery::bq_project_query(project,v1_query)
-  tb2 <- bigrquery::bq_project_query(project,v2_query)
+  ## the dev data from module 1 is empty
+  ## so just return an empty tibble if you are looking
+  ## at the dev data.
+  if (env != "dev"){
+    tbl_v1 <- connect_bigquery_info[[env]]$module1_v1
+    v1_query <- glue::glue_data(cid,"select {Connect_ID},",
+                                "{CurrentJobTitle} as CurrentJobTitle, ",
+                                "{CurrentSelection} as CurrentSelection,",
+                                "{LongestJobTitle} as LongestJobTitle,",
+                                "{LongestSelection} as LongestSelection from {tbl_v1} ",
+                                "where Connect_ID is not null AND not",
+                                "({CurrentJobTitle} is null and {CurrentSelection} is null and ",
+                                "{LongestJobTitle} is null and {LongestSelection} is null)")
+    v1_tbl <- bigrquery::bq_project_query(project,v1_query)
+    v1_data <- bigrquery::bq_table_download(v1_tbl,bigint = "integer64") |>
+      dplyr::mutate(CurrentJobTask = NA_character_,
+             LongestJobTask = NA_character_)
+  } else {
+    v1_data <- tibble::tibble(
+      Connect_ID = bit64::integer64(),
+      CurrentJobTitle = character(),
+      CurrentJobTask = character(),
+      CurrentSelection = character(),
+      LongestJobTitle = character(),
+      LongestJobTask = character(),
+      LongestSelection = character(),
+    )
+  }
 
+  # get the data from version two of module1
+  tbl_v2 <- connect_bigquery_info[[env]]$module1_v2
+  v2_query <- glue::glue_data(cid,
+                      "select {Connect_ID},",
+                      "{CurrentJobTitle} as CurrentJobTitle, ",
+                      "{CurrentJobTask} as CurrentJobTask, ",
+                      "{CurrentSelection} as CurrentSelection, ",
+                      "{LongestJobTitle} as LongestJobTitle, ",
+                      "{LongestJobTask} as LongestJobTask, ",
+                      "{LongestSelection} as LongestSelection from {tbl_v2} " ,
+                      "where Connect_ID is not null AND not",
+                      "({CurrentJobTitle} is null and {CurrentSelection} is null and ",
+                      "{LongestJobTitle} is null and {LongestSelection} is null and ",
+                      "{CurrentJobTask} is null and {LongestJobTask} is null)")
+  v2_tbl <- bigrquery::bq_project_query(project,as.character(v2_query) )
+  v2_data <- bigrquery::bq_table_download(v2_tbl,bigint = "integer64")
+
+  v1_data <- v1_data |> dplyr::select(colnames(v2_data))
   data <- dplyr::bind_rows(
-    bigrquery::bq_table_download(tb1,bigint = "integer64"),
-    bigrquery::bq_table_download(tb2,bigint = "integer64")
+    v1_data,
+    v2_data
   )
 
-  colnames(data) <- c("Connect Id","CurrentJobTitle","CurrentSelection",
-                      "LongestJobTitle","LongestSelection")
   attr(data,"date") <- format(Sys.time(),"%a %b %d %Y %I:%M %p")
   data
 }
@@ -51,7 +125,7 @@ getOccupationData <- function(project=preferences$project,env=preferences$env){
 #'
 #' @param data Occupational data from getOccupationData
 #' @param ...  alternatively, pass in the variables for getOccupationalData and this function
-#'  will call getOccupationalData and pivot in one step
+#'  will call getOccupationalData and pivot in one step (project and env)
 #'
 #' @return a tibble with Connect Id, longest/Current job, JobTitle, Selection, and rank (1-4,5+)
 #' @export
@@ -64,7 +138,9 @@ pivotOccupationData<-function(data,...){
   }
 
   levels =c("1","2","3","4","5+")
-  data <- data %>% tidyr::pivot_longer(2:5, names_to = c("set",".value"), names_sep = 7) %>%
+  data <- data %>% tidyr::pivot_longer(cols=.data$CurrentJobTitle:.data$LongestSelection,
+                                       names_to = c("set",".value"),
+                                       names_pattern = "(Current|Longest)(.*)") %>%
     dplyr::filter(!is.na(.data$JobTitle)&!is.na(.data$Selection)) %>%
     dplyr::filter(grepl("NONE_OF_THE_ABOVE|\\d{2}-\\d{4}-\\d",.data$Selection))  %>%
     dplyr::mutate(rank = factor(dplyr::case_when(
@@ -96,7 +172,7 @@ makeDetailedOccupationBarChart<-function(pivottedData,...){
   pivottedData %>% ggplot2::ggplot(ggplot2::aes(x = rank)) + ggplot2::geom_bar() +
     ggplot2::geom_text(stat = "count",
                        ggplot2::aes(label = ggplot2::after_stat( paste0(!!str2lang("count")," (", round(!!str2lang("count") /sum(!!str2lang("count")) * 100, 1), "%)"))),
-                       vjust = -1) +
+                       vjust = -1,hjust=.3) +
     ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0,0.1))) +
     ggplot2::labs(title = "Connect Participants Selecting from the Top 4\nSOCcer choices",
                   subtitle = "Current and Longest Job",
@@ -140,9 +216,57 @@ makeOccupationBarChart <- function(occData){
                   )
 }
 
-# coming soon...
-#prod_env <- new.env(parent = emptyenv())
-#prod_env$module1 <- "`nih-nci-dceg-connect-prod-6d04.flat.module1_scheduledqueries`"
+#' getIndustryData
+#'
+#' @param project - The project which is billed for the BQ processing.
+#' @param env  - Either  "dev", "stage", or "prod"
+#'
+#' @return - tibble with industry data:
+#'  The Connect_ID, a column of integer64's representingthe Connect_ID;
+#'  CurrentIndustryCID, a list column containing 1 or more ConceptIDs;
+#'  CurrentIndustry, a list column containing 1 or more strings decyphered from the concept id;
+#'  CurrentIndustryOther, a column of characters that represents what a participant put in the
+#'  other field of the the question.
+#'  LongestIndustryCID,LongestIndustry,LongestIndustryOther are the same as the CurrentXX except
+#'  they are for the Longest Job question instead of the current job question.
+#' @export
+#'
+#' @importFrom rlang .data
+#'
+getIndustryData <- function(project=preferences$project,env=preferences$env){
+  if (is.null(project)) stop('project must be defined')
+  if (is.null(env) || !env %in% c("dev","stage","prod")) stop('env must one of "dev", "stage", or "prod"')
+  tbl_v1 <- connect_bigquery_info[[env]]$module1_v1
+  v1_query <- glue::glue_data(cid,"select {Connect_ID},",
+                              "{CurrentIndustry} as CurrentIndustryCID, ",
+                              "{CurrentIndustryOther} as CurrentIndustryOther,",
+                              "{LongestIndustry} as LongestIndustryCID,",
+                              "{LongestIndustryOther} as LongestIndustryOther from {tbl_v1} ",
+                              "where Connect_ID is not null AND ",
+                              "ARRAY_LENGTH({CurrentIndustry}) + ARRAY_LENGTH({LongestIndustry}) > 0")
+  v1_tbl <- bigrquery::bq_project_query(project,v1_query)
+  v1_data <- bigrquery::bq_table_download(v1_tbl,bigint = "integer64")
 
+  tbl_v2 <- connect_bigquery_info[[env]]$module1_v2
+  v2_query <- glue::glue_data(cid,"select {Connect_ID},",
+                              "{CurrentIndustry} as CurrentIndustryCID, ",
+                              "{CurrentIndustryOther} as CurrentIndustryOther,",
+                              "{LongestIndustry} as LongestIndustryCID,",
+                              "{LongestIndustryOther} as LongestIndustryOther from {tbl_v2} ",
+                              "where Connect_ID is not null AND ",
+                              "ARRAY_LENGTH({CurrentIndustry}) + ARRAY_LENGTH({LongestIndustry}) > 0")
+  v2_tbl <- bigrquery::bq_project_query(project,v1_query)
+  v2_data <- bigrquery::bq_table_download(v1_tbl,bigint = "integer64")
+
+
+  dplyr::bind_rows(v1_data,v2_data) |>
+    ## convert the industry CID to the definitions...
+    dplyr::mutate(CurrentIndustry=purrr::map(.data$CurrentIndustryCID,\(x) unname(unlist(cid[x]))),
+                  LongestIndustry=purrr::map(.data$LongestIndustryCID,\(x) unname(unlist(cid[x]))) ) |>
+    ## reorder the columns
+    dplyr::select(.data$Connect_ID,
+                  .data$CurrentIndustryCID,.data$CurrentIndustry,.data$CurrentIndustryOther,
+                  .data$LongestIndustryCID,.data$LongestIndustry,.data$LongestIndustryOther)
+}
 
 
